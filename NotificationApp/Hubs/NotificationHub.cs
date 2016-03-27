67e20685234f2443a8993238ce6d72814e5b0fcd
Diego Bersano <diegobersano33@gmail.com>
2016-03-27 19:07:53 -0300

    Commit inicial, app con funcionalidad básica de marcado de tareas

---
+++ NotificationApp/Hubs/NotificationHub.cs
@@ -0,0 +1,20 @@
+﻿namespace NotificationApp.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+
+    /// <summary>
+    /// Hub de notificaciones de la aplicación.
+    /// </summary>
+    public class NotificationHub : Hub
+    {
+        /// <summary>
+        /// Acción de notificación de una tarea completa.
+        /// </summary>
+        /// <param name="taskId">Identificador de la tarea.</param>
+        /// <param name="taskResult">Resultado de la tarea.</param>
+        public void Notificate(int taskId, bool taskResult)
+        {
+            Clients.All.markDone(taskId, taskResult);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SignalR-Demo
-Proyecto de demostración de SignalR en ASP.NET MVC
+Proyecto de demostración de SignalR en ASP.NET MVC, utilizado en la serie que estoy escribiendo sobre el mismo en [mi blog personal: diegobersano.com.ar](http://diegobersano.com.ar)
